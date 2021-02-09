18753d21b04d1e99f537599ff3b5c983fd0ef62e
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-09 00:08:46 -0600

    Define alphabet characters

--- main/algorithm/shift/shift.go
+++ main/algorithm/shift/shift.go
@@ -11,3 +11,16 @@ type position struct {
 	len  uint
 	curr uint
 }
+
+func getAlphabet() alphabet {
+	// byte array represents ASCII chars
+	// A = 65, ... , Z = 90
+	return alphabet{
+		chars: []byte{
+			'A', 'B', 'C', 'D', 'E', 'F', 'G',
+			'H', 'I', 'J', 'K', 'L', 'M', 'N',
+			'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+			'V', 'W', 'X', 'Y', 'Z',
+		},
+	}
+}
