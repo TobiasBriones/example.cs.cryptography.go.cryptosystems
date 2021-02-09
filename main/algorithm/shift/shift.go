037332c67fee04163dc90f4cc685cece09ee1e61
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-09 00:42:14 -0600

    Implement algorithm

--- main/algorithm/shift/shift.go
+++ main/algorithm/shift/shift.go
@@ -1,5 +1,7 @@
 package shift
 
+import "strings"
+
 const asciiInitialIndex = 65
 
 type alphabet struct {
@@ -12,12 +14,59 @@ type position struct {
 	curr uint
 }
 
+func Encrypt(msg string, key uint) string {
+	var input = getInput(msg)
+	var shifted = shiftRight(input, key)
+	return strings.ToLower(shifted)
+}
+
+func Decrypt(enc string, key uint) string {
+	var input = getInput(enc)
+	var shifted = shiftLeft(input, key)
+	return getOutput(shifted)
+}
+
 func (al alphabet) length() uint {
 	return uint(len(al.chars))
 }
 
 func (al alphabet) positionOf(ch byte) uint {
 	return uint(ch) - asciiInitialIndex
+}
+
+func getInput(msg string) string {
+	var input = strings.ToUpper(msg)
+	return strings.ReplaceAll(input, " ", "")
+}
+
+func getOutput(msg string) string {
+	return strings.ToLower(msg)
+}
+
+func shiftRight(msg string, key uint) string {
+	return shift(msg, int(key))
+}
+
+func shiftLeft(msg string, key uint) string {
+	return shift(msg, -int(key))
+}
+
+func shift(str string, key int) string {
+	var alphabet = getAlphabet()
+	var chars = alphabet.chars
+	var shifted = ""
+	var position = position{
+		key:  key,
+		len:  alphabet.length(),
+		curr: 0,
+	}
+	for _, ch := range str {
+		position.curr = alphabet.positionOf(byte(ch))
+		var newPos = getPosition(position)
+
+		shifted += string(chars[newPos])
+	}
+	return shifted
 }
 
 func getAlphabet() alphabet {
@@ -31,4 +80,16 @@ func getAlphabet() alphabet {
 			'V', 'W', 'X', 'Y', 'Z',
 		},
 	}
+}
+
+func getPosition(pos position) uint {
+	var length = int(pos.len)
+	var newPos = int(pos.curr) + pos.key
+
+	if newPos >= length {
+		newPos -= length
+	} else if newPos < 0 {
+		newPos = length + newPos
+	}
+	return uint(newPos)
 }
