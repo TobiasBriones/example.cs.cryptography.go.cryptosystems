5c887c3a404ea3784d3d2b8d73b770a30dca9a2f
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-09 00:00:44 -0600

    Add encryption tests

--- main/algorithm/shift/shift_test.go
+++ main/algorithm/shift/shift_test.go
@@ -1,1 +1,25 @@
 package shift
+
+import "testing"
+
+func TestOneWordMsgEncrypt(t *testing.T) {
+	const key = 3
+	const msg = "message"
+	const expected = "phvvdjh"
+	var actual = Encrypt(msg, key)
+
+	if actual != expected {
+		t.Fatalf(`Encryption of %q wrong: %q`, msg, actual)
+	}
+}
+
+func TestTwoWordMsgEncrypt(t *testing.T) {
+	const key = 5
+	const msg = "Hello World"
+	const expected = "mjqqtbtwqi"
+	var actual = Encrypt(msg, key)
+
+	if actual != expected {
+		t.Fatalf(`Encryption of %q wrong: %q`, msg, actual)
+	}
+}
