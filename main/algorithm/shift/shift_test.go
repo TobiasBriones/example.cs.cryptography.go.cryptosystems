777db34d64730b64d681e87db588980896a550da
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-09 00:03:58 -0600

    Add decryption tests

--- main/algorithm/shift/shift_test.go
+++ main/algorithm/shift/shift_test.go
@@ -13,6 +13,17 @@ func TestOneWordMsgEncrypt(t *testing.T) {
 	}
 }
 
+func TestOneWordMsgDecrypt(t *testing.T) {
+	const key = 3
+	const enc = "phvvdjh"
+	const expected = "message"
+	var actual = Decrypt(enc, key)
+
+	if actual != expected {
+		t.Fatalf(`Decryption of %q wrong: %q`, enc, actual)
+	}
+}
+
 func TestTwoWordMsgEncrypt(t *testing.T) {
 	const key = 5
 	const msg = "Hello World"
@@ -21,5 +32,16 @@ func TestTwoWordMsgEncrypt(t *testing.T) {
 
 	if actual != expected {
 		t.Fatalf(`Encryption of %q wrong: %q`, msg, actual)
+	}
+}
+
+func TestTwoWordMsgDecrypt(t *testing.T) {
+	const key = 5
+	const enc = "mjqqtbtwqi"
+	const expected = "helloworld"
+	var actual = Decrypt(enc, key)
+
+	if actual != expected {
+		t.Fatalf(`Decryption of %q wrong: %q`, enc, actual)
 	}
 }
