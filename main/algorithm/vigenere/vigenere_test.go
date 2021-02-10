7b1a0e5cbd159ab90cb897d58466bc82f63393ff
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-10 13:48:29 -0600

    Add vigenere tests for words with whitespaces

--- main/algorithm/vigenere/vigenere_test.go
+++ main/algorithm/vigenere/vigenere_test.go
@@ -30,10 +30,36 @@ func TestOneWordDecrypt(t *testing.T) {
 	}
 }
 
+func TestWordsWithWhitespacesEncrypt(t *testing.T) {
+	var sample = getSampleWithWhistespaces()
+	var actual = Encrypt(sample.msg, sample.key)
+
+	if actual != sample.enc {
+		t.Fatalf(`Encryption of %q wrong: %q`, sample.msg, actual)
+	}
+}
+
+func TestWordsWithWhitespacesDecrypt(t *testing.T) {
+	var sample = getSampleWithWhistespaces()
+	var actual = Decrypt(sample.enc, sample.key)
+
+	if actual != sample.msg {
+		t.Fatalf(`Decryption of %q wrong: %q`, sample.enc, actual)
+	}
+}
+
 func getSample() sample {
 	return sample{
 		key: []byte{'C', 'I', 'P', 'H', 'E', 'R'},
 		msg: "thiscryptosystemisnotsecure",
+		enc: "vpxzgiaxivwpubttmjpwizitwzt",
+	}
+}
+
+func getSampleWithWhistespaces() sample {
+	return sample{
+		key: []byte{'C', 'I', 'P', 'H', 'E', 'R'},
+		msg: "This cryptosystem is not secure",
 		enc: "vpxzgiaxivwpubttmjpwizitwzt",
 	}
 }
