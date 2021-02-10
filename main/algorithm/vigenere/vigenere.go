b29c275da86714d1de51991884124b06712e59d5
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-09 23:49:38 -0600

    Implement vigenere Encrypt algorithm

--- main/algorithm/vigenere/vigenere.go
+++ main/algorithm/vigenere/vigenere.go
@@ -1,1 +1,23 @@
 package vigenere
+
+import "strings"
+import "../../algorithm"
+
+func Encrypt(msg string, key []byte) string {
+	var enc = ""
+	var length = len(key)
+	var input = strings.ToUpper(msg)
+	var alphabet = algorithm.GetAlphabet()
+	var m = byte(alphabet.Length())
+
+	for i, ch := range input {
+		var code = alphabet.CanonicalPositionOf(byte(ch))
+		var groupIndex = i % length
+		var keyCh = key[groupIndex]
+		var add = alphabet.CanonicalPositionOf(keyCh)
+		var summation = (byte(code) + byte(add)) % m
+		enc += string(alphabet.Chars[summation])
+	}
+	enc = strings.ToLower(enc)
+	return enc
+}
