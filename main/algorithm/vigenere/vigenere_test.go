daa4cc6872cd4ffb6d56fd1c7521719f43d0fa68
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-09 23:35:19 -0600

    Add vigenere sample Encrypt/Decrypt tests

--- main/algorithm/vigenere/vigenere_test.go
+++ main/algorithm/vigenere/vigenere_test.go
@@ -1,1 +1,39 @@
 package vigenere
+
+import "testing"
+
+type sample struct {
+	key []byte
+	msg string
+	enc string
+}
+
+func (s *sample) length() int {
+	return len(s.key)
+}
+
+func TestOneWordEncrypt(t *testing.T) {
+	var sample = getSample()
+	var actual = Encrypt(sample.msg, sample.key)
+
+	if actual != sample.enc {
+		t.Fatalf(`Encryption of %q wrong: %q`, sample.msg, actual)
+	}
+}
+
+func TestOneWordDecrypt(t *testing.T) {
+	var sample = getSample()
+	var actual = Decrypt(sample.enc, sample.key)
+
+	if actual != sample.msg {
+		t.Fatalf(`Decryption of %q wrong: %q`, sample.enc, actual)
+	}
+}
+
+func getSample() sample {
+	return sample{
+		key: []byte{'C', 'I', 'P', 'H', 'E', 'R'},
+		msg: "thiscryptosystemisnotsecure",
+		enc: "vpxzgiaxivwpubttmjpwizitwzt",
+	}
+}
--- main/main.go
+++ main/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+import "./algorithm"
 
 func main() {
 	fmt.Println("Hello World")
+	var a = []byte{
+		21, 15, 23, 25, 6, 8, 0, 23, 8, 21, 22, 15, 20, 1, 19, 19, 12, 9, 15, 22, 8, 25, 8, 19, 22, 25, 19,
+	}
+	var al = algorithm.GetAlphabet()
+	var enc = ""
+
+	for _, v := range a {
+		enc += string(al.Chars[v])
+	}
+	enc = strings.ToLower(enc)
+	fmt.Println(enc)
 }
