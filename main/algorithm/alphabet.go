7eab4ef93eee2b978dbd660f1a338626c82740c7
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-08-30 18:14:46 -0600

    Add copyright license header

--- main/algorithm/alphabet.go
+++ main/algorithm/alphabet.go
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This file is part of Example Project: Cryptosystems.
+ *
+ * This source code is licensed under the BSD-3-Clause License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/BSD-3-Clause.
+ */
+
 package algorithm
 
 const asciiInitialIndex = 65
